b80217399ad82335750ebc5e7919766be5321f2e
cassiano yasumitsu <cassiano@usuarioincomum.com>
2024-07-31 08:23:23 +0900

    initial commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,11 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def sms
+    phone_number = current_user.phone
+    message = current_user.email
+    TwilioClient.new.send_message(to: phone_number, body: message)
+    redirect_to root_path, notice: "Message sent!"
+  end
 end
---
+++ app/services/twilio_client.rb
@@ -0,0 +1,13 @@
+class TwilioClient
+  def initialize
+    @client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
+  end
+
+  def send_message(to:, body:)
+    @client.messages.create(
+      from: ENV['TWILIO_PHONE_NUMBER'],
+      to: to,
+      body: body
+    )
+  end
+end
