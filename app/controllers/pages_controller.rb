36da8689d3e9e81869dbcbb9c9a35bf711a5e4ce
cassiano yasumitsu <cassiano@usuarioincomum.com>
2024-07-31 21:26:20 +0900

    feat: adding personal message to be send

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,15 +6,15 @@ class PagesController < ApplicationController
 
   def sms
     phone_number = current_user.phone
-    message = current_user.email
+    message = params[:message]
     TwilioClient.new.send_message(to: phone_number, body: message)
-    redirect_to root_path, notice: "Message sent!"
+    redirect_to root_path, notice: "SMS sent!"
   end
 
   def whatsapp
     phone_number = current_user.phone
-    message = current_user.email
+    message = params[:message]
     TwilioClient.new.send_whatsapp(to: phone_number, body: message)
-    redirect_to root_path, notice: "Message sent!"
+    redirect_to root_path, notice: "Whatsapp sent!"
   end
 end
